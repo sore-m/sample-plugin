2218b753107223e3bb79936f02bf75e441fcf869
BuildTools <unconfigured@null.spigotmc.org>
2024-03-08 19:16:42 +0900

    📖
    Signed-off-by: BuildTools <unconfigured@null.spigotmc.org>

--- src/main/kotlin/com/github/sore/sample/plugin/EventListener.kt
+++ src/main/kotlin/com/github/sore/sample/plugin/EventListener.kt
@@ -6,7 +6,8 @@ import net.kyori.adventure.text.format.TextColor
 import net.kyori.adventure.text.format.TextDecoration
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
-import java.util.Calendar
+import org.bukkit.event.player.PlayerCommandPreprocessEvent
+import java.util.*
 import kotlin.random.Random.Default.nextInt
 
 class EventListener(
@@ -18,7 +19,25 @@ class EventListener(
         e.numPlayers = c.get(Calendar.YEAR) * 10000 + (c.get(Calendar.MONTH) + 1) * 100 + c.get(Calendar.DAY_OF_MONTH)
         e.maxPlayers = c.get(Calendar.HOUR) * 10000 + c.get(Calendar.MINUTE) * 100 + c.get(Calendar.SECOND)
         e.motd(
-            Component.keybind("Hello World!").color(TextColor.color(nextInt(0xFFFFF))).decorate(TextDecoration.BOLD)
+            Component.keybind("motd").color(TextColor.color(nextInt(0xFFFFF))).decorate(TextDecoration.BOLD)
         )
+    }
+
+    @EventHandler
+    fun onPlayerCommand(event: PlayerCommandPreprocessEvent) {
+        val command = event.message.split(" ")[0].lowercase(Locale.getDefault())
+
+        if (command == "/plugins") {
+            val errorMessage = Component.text("해당 명령어는 차단되어 있습니다.")
+                .color(TextColor.fromHexString("#FF0000"))
+            event.player.sendMessage(errorMessage)
+            event.isCancelled = true
+        }
+        if (command == "/pl") {
+            val errorMessage = Component.text("해당 명령어는 차단되어 있습니다.")
+                .color(TextColor.fromHexString("#FF0000"))
+            event.player.sendMessage(errorMessage)
+            event.isCancelled = true
+        }
     }
 }
